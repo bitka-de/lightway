64413ec565565b14f2e93199aa31c5eefeda0acf
bitka-de <behrens.ka@icloud.com>
2025-05-03 16:54:40 +0200

    Implement Container class with binding and retrieval methods

--- src/Core/Container.php
+++ src/Core/Container.php
@@ -5,4 +5,19 @@ namespace Lightway\Core;
 class Container
 {
     public function __construct() {}
+    protected array $bindings = [];
+
+    public function bind(string $key, callable $resolver): void
+    {
+        $this->bindings[$key] = $resolver;
+    }
+
+    public function get(string $key)
+    {
+        if (!isset($this->bindings[$key])) {
+            throw new \Exception("No binding found for key: {$key}");
+        }
+
+        return $this->bindings[$key]();
+    }
 }
